2f0c2a68889bf7bb18aec55fa046fe025f1685f6
PORTATIL21 <PORTATIL21@PORTATIL21-PC>
2017-05-04 09:45:27 +0200

    Añadir productos predeterminados

--- Almacen/Formularios/frmAlmacen.vb
+++ Almacen/Formularios/frmAlmacen.vb
@@ -1,3 +1,13 @@
-﻿Public Class frmAlmacen
+﻿Imports CapaDatos
 
+Public Class frmAlmacen
+    Dim nombresProductos As String() = {"Cinta", "Grapadora", "Lapices", "Libros", "Lupa", "Maletin", "Maquina", "Reloj", "Telefono"}
+    Public Property nuestroAlmacen As List(Of Producto)
+    Private Sub frmAlmacen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Creamos los productos predeterminados
+        For x = 0 To 9
+            Dim nuevoPorducto As New Producto(nombresProductos(x), 50)
+            nuestroAlmacen.Add(nuevoPorducto)
+        Next
+    End Sub
 End Class
