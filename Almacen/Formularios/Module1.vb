df85bbb4075d637b53fbaf9c4566389508244cb4
Adrián Reina Goñi <aereoforce@gmail.com>
2017-05-05 12:56:26 +0200

    productos

--- Almacen/Formularios/Module1.vb
+++ Almacen/Formularios/Module1.vb
@@ -2,4 +2,5 @@
 
 Module Module1
     Public listUsus As New Usus
+    Public nuestroAlmacen As New List(Of Producto)
 End Module
--- Almacen/Formularios/frmAlmacen.vb
+++ Almacen/Formularios/frmAlmacen.vb
@@ -2,12 +2,12 @@
 
 Public Class frmAlmacen
     Dim nombresProductos As String() = {"Cinta", "Grapadora", "Lapices", "Libros", "Lupa", "Maletin", "Maquina", "Reloj", "Telefono"}
-    Public Property nuestroAlmacen As List(Of Producto)
+
     Private Sub frmAlmacen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Creamos los productos predeterminados
         For x = 0 To 9
-            Dim nuevoPorducto As New Producto(nombresProductos(x), 50)
-            nuestroAlmacen.Add(nuevoPorducto)
+            Dim nuevoProducto As New Producto(nombresProductos(x), 50)
+            nuestroAlmacen.Add(nuevoProducto)
         Next
     End Sub
 End Class
