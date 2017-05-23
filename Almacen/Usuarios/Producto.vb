0ffe53369a432a83e4145c1ac1bd9538ce656f9c
Adrián Reina Goñi <aereoforce@gmail.com>
2017-05-23 12:13:41 +0200

    Cambios clases stock

--- Almacen/Usuarios/Producto.vb
+++ Almacen/Usuarios/Producto.vb
@@ -1,11 +1,16 @@
-﻿Imports CapaDatos
-
+﻿
 Public Class Producto : Implements IEquatable(Of Producto)
-    Dim ListaProductos As New List(Of Producto)
     Public Property Nombre As String
+    Private _Stock As Integer
     Public Property Stock As Integer
+        Get
+            Return _Stock
+        End Get
+        Set(value As Integer)
+            _Stock = value
+        End Set
+    End Property
     Public Property CantPedida As Integer
-
 
     Public Function Restock(ByVal cantidadPedida As Integer) As String
         Stock += cantidadPedida
@@ -14,49 +19,10 @@ Public Class Producto : Implements IEquatable(Of Producto)
     Public Overloads Function Equals(other As Producto) As Boolean Implements IEquatable(Of Producto).Equals
         Return other IsNot Nothing AndAlso Me.Nombre.ToUpper = other.Nombre.ToUpper
     End Function
-    Public Sub New(nombre As String, stock As Integer)
-        CantPedida = 0
+    Public Sub New(nombre As String)
         Me.Nombre = nombre
-        Me.Stock = stock
+        Me.Stock = 50
     End Sub
-    Public Function CambiarStock(ProductoPedido As Producto, cantPedida As Integer) As String
-        Dim comprobacion As Boolean = False
-        Dim PosAlmacen As Integer
-        For i = 0 To ListaProductos.Count - 1
-            If ListaProductos(i).Equals(ProductoPedido) Then
-                comprobacion = True
-                PosAlmacen = i
-            End If
-        Next
 
-        If comprobacion = True Then
-            ListaProductos(PosAlmacen).Stock = ListaProductos(PosAlmacen).Stock - cantPedida
-        Else
-            ListaProductos.Add(ProductoPedido)
-            ListaProductos(ListaProductos.Count - 1).Stock = 50 - cantPedida
-        End If
-
-
-    End Function
-
-
-    Public Function RenovarStock() As String
-        For i = 0 To ListaProductos.Count - 1
-            ListaProductos(i).Stock = 50
-        Next
-    End Function
-
-
-
-    Public Function ComprobarAlmacen(ByVal producto As Producto, ByVal cantidad As Integer) As Boolean
-        For i = 0 To ListaProductos.Count - 1
-            If ListaProductos(i).Equals(producto) Then
-                If ListaProductos(i).Stock < cantidad Then
-                    Return True
-                End If
-            End If
-        Next
-        Return False
-    End Function
 
 End Class
